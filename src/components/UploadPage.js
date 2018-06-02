d36d5b48aa10fdc9aea99a1344d83a401b66b4dd
cicomsa <ioanacomsa85@gmail.com>
2018-06-02 13:18:46 +0300

    upload form added

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -2,10 +2,38 @@ import React, {PureComponent} from 'react'
 
 
 export default class UploadPage extends PureComponent {
+    state = {file: ''}
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state.file)
+    }
+
+    handleImageChange = (e) => {
+        e.preventDefault()
+        let file = e.target.files[0]
+        this.setState({
+            file: file
+        })
+        console.log('file added')
+    }    
+
     render() {
+
         return (
             <div>
-                <p>Hello</p>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        className="fileInput"
+                        type="file"
+                        onChange={(e)=>this.handleImageChange(e)} />
+                    <button 
+                        className="submitButton"
+                        type="submit"
+                        onClick={(e)=>this.handleSubmit(e)}>
+                        Upload Image
+                    </button>
+                </form>
             </div>
         )
     }
