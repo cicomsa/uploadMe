e29325408fb3b85ff995e90fd246db89819d815a
cicomsa <ioanacomsa85@gmail.com>
2018-06-02 12:56:00 +0300

    UploadPage created

---
+++ src/components/UploadPage.js
@@ -0,0 +1,12 @@
+import React, {PureComponent} from 'react'
+
+
+export default class UploadPage extends PureComponent {
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        )
+    }
+}
