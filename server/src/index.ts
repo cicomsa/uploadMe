379796aafe6c3e6ff6a7ad584fcdf52ed1f39b1b
cicomsa <ioanacomsa85@gmail.com>
2018-06-04 13:56:32 +0300

    kcors added

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,12 @@ import setupDb from './db'
 const port = process.env.PORT || 4000
 
 const app = createKoaServer({
-   controllers: [ImageController]
+    cors: true,
+    controllers: [ImageController]
 })
 
 setupDb()
-  .then(_ =>
-    app.listen(4000, () => console.log('Listening on port ' + port))
-  )
-  .catch(err => console.error(err))
+    .then(_ =>
+        app.listen(4000, () => console.log('Listening on port ' + port))
+    )
+    .catch(err => console.error(err))
