4049f9353b56dfd8f73c6e70a9f16d877492663b
cicomsa <ioanacomsa85@gmail.com>
2018-06-04 12:53:00 +0300

    removed constructor and css for full screen only

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,18 +1,14 @@
 import React, {PureComponent} from 'react'
 
 export default class UploadForm extends PureComponent {
-
-    constructor(props) {
-        super(props)
-        this.state = {file: '',imagePreviewUrl: ''}
-    }
-     
+   
+    state = {file: '',imagePreviewUrl: ''}
+       
     handleSubmit = (e) => {
         e.preventDefault();
   
         this.props.onSubmit(this.state)
         this.setState({
-            file: "",
             imagePreviewUrl: ""
         })
     }
@@ -42,12 +38,14 @@ export default class UploadForm extends PureComponent {
         return (
           <div className="previewComponent">    
             <form onSubmit={(e)=>this.handleSubmit(e)}>
-              <input className="fileInput"
-                type="file"
-                onChange={(e)=>this.handleImageChange(e)} />
-              <button className="submitButton"
-                type="submit"
-                onClick={(e)=>this.handleSubmit(e)}>Upload Image</button>
+                <input className="fileInput"
+                    type="file"
+                    onChange={(e)=>this.handleImageChange(e)} />
+                <button className="submitButton"
+                    type="submit"
+                    onClick={(e)=>this.handleSubmit(e)}>
+                    Upload Image
+                 </button>
             </form>
             <div className="imgPreview">
                 {imagePreview}
